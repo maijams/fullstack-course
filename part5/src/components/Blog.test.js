71b9edd28c098de36b8a16e58d5caa0f81111020
Maija <maija.saisa@helsinki.fi>
2024-01-21 15:05:32 +0200

    Ex. 5.14-5.15

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -10,7 +10,19 @@ describe('<Blog />', () => {
     title: 'React patterns',
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
-    likes: 7
+    likes: 7,
+    user: {
+      username: 'test3',
+      name: 'test',
+      id: '65a8f6843fc6bb265cf516d8'
+    },
+    id: '65abee845578955439861ffa'
+  }
+
+  const testuser = {
+    username: 'test3',
+    name: 'test',
+    id: '65a8f6843fc6bb265cf516d8'
   }
 
   test('renders content', () => {
@@ -21,6 +33,36 @@ describe('<Blog />', () => {
 
     expect(screen.queryByText('https://reactpatterns.com/', { exact: false })).toBeNull()
     expect(screen.queryByText('7', { exact: false })).toBeNull()
+  })
+
+  test('clicking the "view"-button reveals more information', async () => {
+    render(<Blog blog={blog} user={testuser} />)
+
+    const user = userEvent.setup()
+
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    screen.findByText('https://reactpatterns.com/')
+    screen.findByText('7')
+    screen.findByText('test')
+  })
+
+  test('clicking the "like"-button twice calls event handler twice', async () => {
+    const mockHandler = jest.fn()
+
+    render(<Blog blog={blog} user={testuser} updateLikes={mockHandler} />)
+
+    const user = userEvent.setup()
+
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
 
