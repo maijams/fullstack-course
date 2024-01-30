b8508996986a3a434dc2d8b02ae9d66bdfa33d76
Maija <maija.saisa@helsinki.fi>
2024-01-30 12:38:13 +0200

    Ex. 8.3

--- part8/index.js
+++ part8/index.js
@@ -99,17 +99,21 @@ const typeDefs = `
     published: Int!
     author: String!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
     name: String!
     born: Int
+    bookCount: Int!
+    id: ID!
   }
 
   type Query {
     bookCount: Int
     authorCount: Int
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -118,6 +122,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+
+  Author: {
+    bookCount: (root) => {
+      return books.filter(book => book.author === root.name).length
+    }
   }
 }
 
