1fec7dabc089a553a4ebc58ce1615e7f2cda8c17
Maija <maija.saisa@helsinki.fi>
2024-02-01 19:37:54 +0200

    Ex. 9.7

--- part9/index.ts
+++ part9/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 const app = express();
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -14,6 +17,20 @@ app.get('/bmi', (req, res) => {
   }
   const bmi = calculateBmi(height, weight);
   return res.json({ weight, height, bmi });
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+  if (!daily_exercises || !target) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+  if (!Array.isArray(daily_exercises) || daily_exercises.some(hours => isNaN(Number(hours))) || isNaN(Number(target))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculateExercises(daily_exercises, target);
+  return res.json(result);
 });
 
 const PORT = 3003;
