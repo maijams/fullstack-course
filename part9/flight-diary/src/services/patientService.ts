59a01863c8539f2a68b7a41cae5b9d7c4328a4c6
Maija <maija.saisa@helsinki.fi>
2024-02-04 17:07:44 +0200

    Ex. 9.12-9.13

--- part9/flight-diary/src/services/patientService.ts
+++ part9/flight-diary/src/services/patientService.ts
@@ -1,5 +1,8 @@
 import patientData from '../../data/patients';
-import { NonSensitivePatient } from '../types';
+import { NonSensitivePatient, Patient, NewPatient } from '../types';
+import { v1 as uuid } from 'uuid';
+
+const patients: Patient[] = patientData;
 
 const getPatients = (): NonSensitivePatient[] => {
   return patientData.map(({ id, name, dateOfBirth, gender, occupation }) =>
@@ -12,7 +15,20 @@ const getPatients = (): NonSensitivePatient[] => {
   }));
 };
 
+const addPatient = ( entry: NewPatient ): Patient => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  // eslint-disable-next-line
+  const id: string = uuid();
+  const newPatient = {
+    id,
+    ...entry
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
 
 export default {
-  getPatients
+  getPatients,
+  addPatient
 };
--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -4,13 +4,21 @@ export type Diagnosis = {
   latin?: string;
 };
 
-export type Patient = {
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
+}
+
+export interface Patient {
   id: string;
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
-};
+}
+
+export type NewPatient = Omit<Patient, 'id'>;
 
 export type NonSensitivePatient = Omit<Patient, 'ssn'>;
