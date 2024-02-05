6ecf0d88e2b35b2a86a0155cf7ddb07409a01276
Maija <maija.saisa@helsinki.fi>
2024-02-05 14:01:47 +0200

    Ex. 9.16

--- part9/flight-diary/client/src/App.tsx
+++ part9/flight-diary/client/src/App.tsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Diary {
+  id: number;
+  date: string;
+  visibility: string;
+  weather: string;
+}
+
+const App = () => {
+  const [diaries, setDiaries] = useState<Diary[]>([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/diaries').then(response => {
+      setDiaries(response.data);
+    })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Diary entries</h2>
+        {diaries.map((diary) => 
+          <div key={diary.id}>
+            <h3>{diary.date}</h3>
+            visibility: {diary.visibility}
+            <br/>
+            weather: {diary.weather}
+          </div>
+        )}
     </>
   )
 }
